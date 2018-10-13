e41bc1e204275cb1654040ff47d527340c3b1d25
Sushil Srinatha <sushisri10@gmail.com>
2018-10-12 22:55:34 -0400

    Added basic links & firebase support

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var config = {
+  apiKey: "AIzaSyB7GyXDFZNrg-LGUGIE6RyXwHmvtB3ipoQ",
+  authDomain: "mhacksproject.firebaseapp.com",
+  databaseURL: "https://mhacksproject.firebaseio.com",
+  projectId: "mhacksproject",
+  storageBucket: "mhacksproject.appspot.com",
+  messagingSenderId: "294027637255"
+};
+firebase.initializeApp(config);
